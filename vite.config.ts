7326cb9dabfc4ddabeeb04b8a9e90be0ca61aef5
stepan <steveruu@stvr.cz>
2025-05-16 21:26:40 +0200

    chore: pwa ready

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,47 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    server: {
+        host: "::",
+        port: 8080,
     },
-  },
+    plugins: [
+        react(),
+        mode === "development" && componentTagger(),
+        VitePWA({
+            registerType: "autoUpdate",
+            includeAssets: [
+                "favicon.ico",
+                "apple-touch-icon.png",
+                "masked-icon.svg",
+            ],
+            manifest: {
+                name: "f1.stvr.cz by steveruu",
+                short_name: "f1.stvr.cz",
+                description: "Kompletní kalendář závodů a výsledky",
+                theme_color: "#ffffff",
+                icons: [
+                    {
+                        src: "pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
+    ].filter(Boolean),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 }));
