5be24e2eae690c12f52b681f7ef6fc2ecd280eb5
stepan <steveruu@stvr.cz>
2025-05-16 21:34:04 +0200

    chore: rebuilt assets without padding

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -1,9 +1,26 @@
 import {
     defineConfig,
+    Preset,
     minimal2023Preset as preset,
 } from "@vite-pwa/assets-generator/config";
 
+export const minimalPresetNoPadding: Preset = {
+    transparent: {
+        sizes: [64, 192, 512],
+        favicons: [[48, "favicon.ico"]],
+        padding: 0,
+    },
+    maskable: {
+        sizes: [512],
+        padding: 0,
+    },
+    apple: {
+        sizes: [180],
+        padding: 0,
+    },
+};
+
 export default defineConfig({
-    preset,
+    preset: minimalPresetNoPadding,
     images: ["public/logo.png", "public/logo.png"],
 });
